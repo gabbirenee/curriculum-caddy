80ec10dc9491f0870008f9e52fa0592fd4a4e5c8
Gabbi Forsythe <gforsythe17@gmail.com>
2024-10-22 17:54:51 -0400

    Basic Markdown capabilities - need to work on code rendering

--- src/Components/BotMessage.js
+++ src/Components/BotMessage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 function BotMessage ({text}) {
   return (
     <div className='chat-message-bot'>
-      {text}
+      <ReactMarkdown>{text}</ReactMarkdown>
     </div>
   );
 }

9f65e959ad8fb734e3ea04ec31daae90850dcc32
Gabbi Forsythe <gforsythe17@gmail.com>
2024-10-23 22:14:38 -0400

    Added code block formatting to bot responses

--- src/Components/BotMessage.js
+++ src/Components/BotMessage.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 function BotMessage ({text}) {
   return (
     <div className='chat-message-bot'>
-      <ReactMarkdown>{text}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        children={text}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                style={dracula} // theme
+                language={match[1]}
+                PreTag='section' // parent tag
+                {...props}
+              />
+            ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+            );
+          },
+        }}
+      />
     </div>
   );
 }

721f19e73925ac1101a42276abe0f95622ac70c4
Gabbi Forsythe <gforsythe17@gmail.com>
2024-11-05 20:49:09 -0500

    Added Home button to settings page

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import Curriculum from './Curriculum.js';
 
 function Settings () {
+  const navigate = useNavigate(); 
+
+  const handleClick = () => {
+    console.log("Home Button Clicked");
+    navigate('/');
+  }
+
   return (
     <div className="Settings">
+      <button onClick={handleClick}>Home</button>
       <p>Settings</p>
       <Curriculum />
     </div>

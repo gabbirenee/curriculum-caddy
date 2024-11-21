2c08064c186c386e7e732652336787e700b0eaf4
Gabbi Forsythe <gforsythe17@gmail.com>
2024-11-20 22:02:52 -0500

    Style inputs for document details

--- src/Components/DocumentDetails.js
+++ src/Components/DocumentDetails.js
@@ -181,38 +181,39 @@ function DocumentDetails ({subject, grade_level, curriculum, setCurriculum, sele
 
   return (
     <div className="DocumentDetails">
-      <div>
+      <div className="details-section">
         <h1>Document Details</h1>
         <div className="file-upload-container">
           <input type="file" accept=".pdf" ref={inputRef} onChange={(e) => onFileLoad(e)} />
         </div>
         <div className="current-doc-details">
-          <h3>Document Name</h3>
+          <h4 className="inline-header">Document Name</h4>
           <input
             value={docName}
             onChange={(e)=> setDocName(e.target.value)}
             placeholder="Example: Unit 1, Lesson 1"
           />
-          <h3>Document Status</h3>
+          <div />
+          <h4 className="inline-header">Document Status</h4>
           <select value={docStatus} onChange={(e)=> setDocStatus(e.target.value)}>
             <option value="not-started">Not Started</option>
             <option value="in-progress">In Progress</option>
             <option value="completed">Completed</option>
           </select>
-          <h3>Key Objectives</h3>
+          <h4>Key Objectives</h4>
           <textarea
             value={keyObj}
             onChange={(e)=> setKeyObj(e.target.value)}
             placeholder="Add any key goals, objectives, or skills to learn."
           />
-          <h3>Key Terms</h3>
+          <h4>Key Terms</h4>
           <textarea
             value={keyTerms}
             onChange={(e)=> setKeyTerms(e.target.value)}
             placeholder="Add any key terms that your students should understand for this lesson."
           />
-          <h3>Skill Level</h3>
-          <input
+          <h4 className="inline-header">Skill Level</h4>
+          <input 
             type="number"
             value={skill}
             onChange={(e)=> setSkill(e.target.value)}
@@ -220,9 +221,11 @@ function DocumentDetails ({subject, grade_level, curriculum, setCurriculum, sele
             max="10"
             step="1"
           />
-          <button className="save-doc-changes" onClick={handleSave}>Save</button>
-          <button className="discard-doc-changes" onClick={handleDiscard}>Discard Changes</button>
-          <button className="delete-doc" onClick={handleDelete}>Delete</button>
+          <div className="doc-details-btns">
+            <button className="save-doc-changes" onClick={handleSave}>Save</button>
+            <button className="discard-doc-changes" onClick={handleDiscard}>Discard Changes</button>
+            <button className="delete-doc" onClick={handleDelete}>Delete</button>
+          </div>
         </div>
       </div>
       <div className="add-new-doc" onClick={handleNewDoc}>
